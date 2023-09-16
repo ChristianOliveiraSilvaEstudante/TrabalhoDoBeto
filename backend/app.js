a39d36d03077e0022319b5882aa9c0a48d3ab632
ChristianOliveiraSilva <gamesparamundo123@gmail.com>
2023-09-16 17:04:22 -0400

    init

---
+++ backend/app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const logger = require('morgan');
+const PrismaClient = require('@prisma/client');
+
+const prisma = new PrismaClient()
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+async function main() {
+    
+}
+  
+main()
+    .then(async () => {
+        await prisma.$disconnect()
+    })
+    .catch(async (e) => {
+        console.error(e)
+        await prisma.$disconnect()
+        process.exit(1)
+    })
+
+module.exports = app;
